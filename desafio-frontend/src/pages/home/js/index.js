3b6f41ec1c6c20b88ff4ff83b32c43b31e356c75
ztaaso <ztaaso.cedaspy@gmail.com>
2021-03-06 22:52:45 -0300

    dark mode implemented

---
+++ desafio-frontend/src/pages/home/js/index.js
@@ -0,0 +1,26 @@
+const addDarkModeListener = () => {
+  const darkModeCheck = document.querySelector('#darkmode');
+  const html = document.querySelector('html');
+  const logo = {
+    img: document.querySelector('#logo'),
+    white_src: '../../../assets/logo.png',
+    dark_src:
+      'https://nave.rs/static/a534b1a1b091cca710d559ef070e9711/3d1ac/logo-white.png',
+  };
+
+  darkModeCheck.addEventListener('click', () => {
+    const isDarkModeOn = darkModeCheck.checked;
+    if (isDarkModeOn) {
+      logo.img.src = logo.dark_src;
+      html.setAttribute('class', 'dark-mode');
+      return;
+    }
+
+    html.removeAttribute('class');
+    logo.img.src = logo.white_src;
+  });
+};
+
+window.onload = () => {
+  addDarkModeListener();
+};
