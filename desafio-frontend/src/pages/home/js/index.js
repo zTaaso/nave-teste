9c2bb56325671673d1ed8085f030764bf3d821ba
ztaaso <ztaaso.cedaspy@gmail.com>
2021-03-08 01:42:56 -0300

    util functions implemented abstractly

--- desafio-frontend/src/pages/home/js/index.js
+++ desafio-frontend/src/pages/home/js/index.js
@@ -1,35 +1,9 @@
-const addDarkModeListener = () => {
-  const darkModeCheck = document.querySelector('#darkmode');
-  const html = document.querySelector('html');
-  const logo = {
-    img: document.querySelector('#logo'),
-    white_src: '../../../assets/logo.png',
-    dark_src:
-      'https://nave.rs/static/a534b1a1b091cca710d559ef070e9711/3d1ac/logo-white.png',
-  };
+import addDarkModeListener from './util/addDarkModeListener.js';
+import fetchNavers from './util/fetchNavers.js';
+import renderNavers from './util/renderNavers.js';
 
-  darkModeCheck.addEventListener('click', () => {
-    const isDarkModeOn = darkModeCheck.checked;
-    if (isDarkModeOn) {
-      logo.img.src = logo.dark_src;
-      html.setAttribute('class', 'dark-mode');
-      return;
-    }
-
-    html.removeAttribute('class');
-    logo.img.src = logo.white_src;
-  });
-};
-
-const fetchNavers = async () => {
-  const api = 'https://my-json-server.typicode.com/naveteam/fake-api/navers';
-  const response = await fetch(api);
-  const navers = await response.json();
-
-  console.log(navers);
-};
-
-window.onload = () => {
+window.onload = async () => {
   addDarkModeListener();
-  fetchNavers();
+  const navers = await fetchNavers();
+  renderNavers(navers);
 };
