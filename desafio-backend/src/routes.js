9077a4d710db0b28213ef792d6d38aa8fd6318e4
ztaaso <ztaaso.cedaspy@gmail.com>
2021-03-10 23:57:22 -0300

    routes added

--- desafio-backend/src/routes.js
+++ desafio-backend/src/routes.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import NaverController from './app/controllers/NaverController';
+import ProjectController from './app/controllers/ProjectController';
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'vsf men' });
-});
+routes.get('/navers', NaverController.index);
+routes.get('/naver/:id', NaverController.show);
+routes.post('/naver', NaverController.store);
+
+routes.get('/projects', ProjectController.index);
+routes.get('/project/:id', ProjectController.show);
+routes.post('/project', ProjectController.store);
 
 export default routes;
